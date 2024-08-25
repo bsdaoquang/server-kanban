6115f61011fee849baed11863f804c413e5e24fb
bsdaoquang <bsdaoquang@gmail.com>
2024-08-25 23:02:37 +0700

    create new account

--- index.ts
+++ index.ts
@@ -4,11 +4,15 @@ import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './src/routers/user';
+import cors from 'cors';
 dotenv.config();
 
 const PORT = process.env.PORT || 3001;
 const dbURL = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.rervmah.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 app.use('/auth', userRouter);
 
--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,13 +1,31 @@
 /** @format */
 
+import UserModel from '../models/UserModel';
+import dotenv from 'dotenv';
+import bcrypt from 'bcrypt';
+dotenv.config();
+
 const register = async (req: any, res: any) => {
 	const body = req.body;
+	const { email, name, password } = body;
 	try {
-		console.log(body);
+		const user = await UserModel.findOne({ email });
+
+		if (user) {
+			throw new Error(`Tài khoản đã tồn tại`);
+		}
+
+		const salt = await bcrypt.genSalt(10);
+		const hashpassword = await bcrypt.hash(password, salt);
+
+		body.password = hashpassword;
+
+		const newUser = new UserModel(body);
+		await newUser.save();
 
 		res.status(200).json({
 			message: 'Register',
-			data: body,
+			data: newUser,
 		});
 	} catch (error: any) {
 		res.status(404).json({
