5c84b2fd38a360e7f1379b075513127bdcd27c19
bsdaoquang <bsdaoquang@gmail.com>
2024-09-10 22:56:17 +0700

    last code

---
+++ src/forms/product.ts
@@ -0,0 +1,25 @@
+/** @format */
+
+export const productForms = [
+	{
+		key: 'price',
+		value: 'title',
+		label: 'Products',
+		placeholder: 'Enter product name',
+		type: 'defautl',
+		required: true,
+		message: 'Enter product name',
+		default_value: '',
+		displayLength: 400,
+	},
+	{
+		key: 'price',
+		value: 'price',
+		label: 'Prive',
+		placeholder: '',
+		type: 'number',
+		message: 'Enter Prive',
+		default_value: '',
+		displayLength: 300,
+	},
+];
---
+++ src/models/ProductModel.ts
@@ -0,0 +1,41 @@
+/** @format */
+
+import mongoose, { Schema } from 'mongoose';
+
+const scheme = new Schema({
+	title: {
+		type: String,
+		required: true,
+	},
+	slug: String,
+	description: String,
+	categories: [String],
+	supplier: {
+		require: true,
+		type: String,
+	},
+	expiryDate: {
+		type: Date,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
+	updatedAt: {
+		type: Date,
+		default: Date.now,
+	},
+});
+
+const ProductModel = mongoose.model('products', scheme);
+export default ProductModel;
+
+/*
+	1 link web đã deploy
+	link github của web đó
+	link github của các bạn 
+
+	zalo / messenger 
+
+	dateline: Chủ nhật
+*/
