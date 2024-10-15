3ebd324405091c1753118476c5e37ca89fc4bd97
bsdaoquang <bsdaoquang@gmail.com>
2024-10-15 21:56:21 +0700

    add promotion apis

--- src/controllers/protions.ts
+++ src/controllers/protions.ts
@@ -33,4 +33,34 @@ const getPromotions = async (req: any, res: any) => {
 	}
 };
 
-export { addNew, getPromotions };
+const update = async (req: any, res: any) => {
+	const { id } = req.query;
+	const body = req.body;
+
+	try {
+		await PromotionModel.findByIdAndUpdate(id, body);
+
+		res.status(200).json({
+			message: 'Updated',
+			data: [],
+		});
+	} catch (error: any) {
+		res.status(404).json({ message: error.message });
+	}
+};
+
+const remove = async (req: any, res: any) => {
+	const { id } = req.query;
+	try {
+		await PromotionModel.findByIdAndDelete(id);
+
+		res.status(200).json({
+			message: 'Deleted',
+			data: [],
+		});
+	} catch (error: any) {
+		res.status(404).json({ message: error.message });
+	}
+};
+
+export { addNew, getPromotions, update, remove };
--- src/routers/promotionRouter.ts
+++ src/routers/promotionRouter.ts
@@ -1,11 +1,13 @@
 /** @format */
 
 import { Router } from 'express';
-import { addNew, getPromotions } from '../controllers/protions';
+import { addNew, getPromotions, remove, update } from '../controllers/protions';
 
 const router = Router();
 
 router.post('/add-new', addNew);
 router.get('/', getPromotions);
+router.put('/update', update);
+router.delete('/remove', remove);
 
 export default router;
